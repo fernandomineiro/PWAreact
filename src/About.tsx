74181a809c46a254339326e9ac675335d251d8c9
fernando <fernandofitilan@hotmail.com>
2022-01-23 19:11:47 -0300

    up

---
+++ src/About.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About: React.FC = () => (
+  <h1>Isso é um PWA</h1>
+);
+
+export default About;
---
+++ src/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home: React.FC = () => (
+  <h1>Bem vindo ao PWA!</h1>
+);
+
+export default Home;
